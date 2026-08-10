38a762f676ae673da783fc8b4ab3c36763ebeb98
Avisek <avisekdulal07@gmail.com>
2026-08-10 15:55:37 +0545

    Weather app with news and toggle theme Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { News } from "./pages/News";
 import { Layout } from "./components/Layout";
 import './App.css'
 function App() {
   return (
-    <BrowserRouter>
+    
       <Routes>
         <Route element={<Layout />} >
             <Route path="/" element={<Home />} />
@@ -13,7 +13,7 @@ function App() {
             <Route path="/news" element={<News />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+  
   );
 }
 
